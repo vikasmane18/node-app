142dfb437f257777af0da6130ccf4ecfc4a96a03
vikasmane18 <vikasmane7620@gmail.com>
2025-11-05 14:37:16 +0530

    added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins & PM2  mane deployed Node.js App through jenkins cicd pipeline!, this is my second chnage');
+  res.send('Hello from Jenkins & PM2 deployed Node.js App through jenkins cicd pipeline!, this is my second chnage');
 });
 
 app.listen(port, () => {
